7948092659e3664a511dbedfb3bb9d57cda217f3
Peyton <pekellogg@hotmail.com>
2022-05-05 16:33:15 -0700

    commit tree f29f4a7a095120e506e06f1c7c8d8576fc887195

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }, presence: true
+    validates :summary, length: { maximum: 250 }, presence: true
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }, presence: true
+    validate :title_non_clickbait
+
+    def title_non_clickbait
+        if [/Won't Believe/, /Secret/, /Top [0-9]*/, /Guess/].none? { |clickbait_exp| clickbait_exp.match(title) }
+            errors.add(:title, "title is not clickbaity-y")
+        end
+    end
 end
+
+# https://stackoverflow.com/questions/23530762/check-whether-a-string-contains-one-of-multiple-substrings
